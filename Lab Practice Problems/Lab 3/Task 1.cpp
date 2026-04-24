6bb30705a4652233263b38eb3fdf16b303b7c8ff
Syed Zia Ur Rehman Hashmi <139067873+zia19068@users.noreply.github.com>
2026-04-24 21:04:57 +0500

    Implement linked list for student management
    This code implements a linked list to manage student records, allowing for adding, displaying, and deleting students.

---
+++ Lab Practice Problems/Lab 3/Task 1.cpp
@@ -0,0 +1,108 @@
+#include<iostream>
+using namespace std;
+class Node{
+	public:
+	string department;
+	string name;
+	int rollnumber;
+	Node* nextptr;
+	Node(string dept,string nam, int no){
+		department=dept;
+		name =nam;
+		rollnumber=no;
+		nextptr=NULL;
+	}
+};
+class Linklist{
+	public:
+	Node* head;
+	Node* tail;
+	Linklist(){
+        head = tail = NULL;
+    }
+    void add_student(string dept, string nam, int no){
+    Node* newnode = new Node(dept, nam, no);
+
+    if(head == NULL){
+        head = tail = newnode;
+        return;
+    }
+
+    tail->nextptr = newnode;
+    tail = newnode;
+}
+	void display(){
+        if(head == NULL){
+            cout << "No students in the list." << endl;
+            return;
+        }
+
+        Node* temp = head;
+        while(temp != NULL){
+            cout << "Department: " << temp->department << endl;
+            cout << "Name: " << temp->name << endl;
+            cout << "Roll Number: " << temp->rollnumber << endl;
+            cout << "-------------------------" << endl;
+            temp = temp->nextptr;
+        }
+    }
+    void delete_student(int roll){
+    	Node* temp = head;
+    	Node* temp1 ;
+    	int idx=0;
+    	while(temp->rollnumber !=roll){
+    		temp=temp->nextptr;
+    		idx++;
+    		}
+    	if(head->rollnumber==roll){
+    		temp = head;
+    		temp1=temp->nextptr;
+	     	head=temp1;
+	    	delete temp;
+	    	return;
+		}
+    	int i;
+    	temp = head;
+    	for(i=0;i<idx-1;i++){
+		temp=temp->nextptr;
+	    }
+	    temp1=temp->nextptr;
+		temp->nextptr=temp1->nextptr;
+		delete temp1;
+	}
+	
+	
+};
+int main(){
+    Linklist students;
+    students.add_student("Computer Science", "Jawad", 101);
+    students.add_student("Electrical Engineering", "Saim", 102);
+    students.add_student("Mechanical Engineering", "Ahmed", 103);
+    students.add_student("Software Engineering", "Fatima", 104);
+
+    cout << "Student List:\n";
+    cout << "=========================\n";
+    students.display();
+    students.delete_student(101);
+    students.delete_student(103);
+    cout << endl;
+    cout << "Updated Student List:\n";
+    cout << "=========================\n";
+    students.display();
+    students.add_student("Applied Physics", "Junaid", 105);
+    students.add_student("Computer Science", "Zaid", 106);
+    students.add_student("Data Science", "Nawaz", 107);
+    cout << endl;
+    cout << "Updated Student List:\n";
+    cout << "=========================\n";
+    students.display();
+    students.delete_student(106);
+    cout << endl;
+    cout << "Updated Student List:\n";
+    cout << "=========================\n";
+    students.display();
+    
+
+    return 0;
+
+}
