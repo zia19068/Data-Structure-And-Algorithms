4505907ae624cd12fc2760c98fb30d006c528974
Syed Zia Ur Rehman Hashmi <139067873+zia19068@users.noreply.github.com>
2026-04-24 20:58:49 +0500

    Add files via upload

---
+++ Lab Practice Problems /Lab 1/Task 2.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Exam {
+    char* name;
+    char* date;
+    double score;
+
+public:
+    Exam() {
+        name = NULL;
+        date = NULL;
+        score = 0;
+    }
+
+    ~Exam() {
+        delete[] name;
+        delete[] date;
+    }
+
+    void setName(const char* n) {
+        delete[] name;
+        name = new char[strlen(n) + 1];
+        strcpy(name, n);
+    }
+
+    void setDate(const char* d) {
+        delete[] date;
+        date = new char[strlen(d) + 1];
+        strcpy(date, d);
+    }
+
+    void setScore(double s) {
+        score = s;
+    }
+
+    void display() {
+    	cout << "------------------" << endl;
+        cout << "Name: " << name << endl;
+        cout << "Date: " << date << endl;
+        cout << "Score: " << score << endl;
+        cout << "------------------" << endl;
+    }
+};
+
+int main() {
+    Exam obj1;
+    obj1.setName("Zohiab");
+    obj1.setDate("12-05-2024");
+    obj1.setScore(85.5);
+
+    cout << "Object 1 Details:" << endl;
+    obj1.display();
+
+    Exam obj2 = obj1; 
+
+    cout << "Object 2 Details (Shallow Copy):" << endl;
+    obj2.display();
+
+    return 0;
+}
