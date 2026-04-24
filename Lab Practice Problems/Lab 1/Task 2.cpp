5c956093c98964b616ab1a17031630d6ffaa7a69
Syed Zia Ur Rehman Hashmi <139067873+zia19068@users.noreply.github.com>
2026-04-24 21:18:54 +0500

    Update Task 2.cpp

--- Lab Practice Problems/Lab 1/Task 2.cpp
+++ Lab Practice Problems/Lab 1/Task 2.cpp
@@ -1,3 +1,8 @@
+/*Q2. Create a C++ class named "Exam" using DMA designed to manage student exam records,
+complete with a shallow copy implementation? Define attributes such as student name, exam
+date, and score within the class, and include methods to set these attributes and display exam
+details. As part of this exercise, intentionally omit the implementation of the copy constructor and copy assignment operator. Afterward, create an instance of the "Exam" class, generate a
+shallow copy, and observe any resulting issues?*/
 #include <iostream>
 #include <cstring>
 using namespace std;
