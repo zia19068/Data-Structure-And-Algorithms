5901d1d4f702570605da0a0040d74d336f4fc1a0
Syed Zia Ur Rehman Hashmi <139067873+zia19068@users.noreply.github.com>
2026-04-24 21:21:15 +0500

    Update Task 2.cpp

--- Lab Practice Problems/Lab 2/Task 2.cpp
+++ Lab Practice Problems/Lab 2/Task 2.cpp
@@ -1,3 +1,11 @@
+/*Task#2
+A school has conducted exams for 5 students in 4 subjects.
+You are required to:
+ Use a appropriate dynamic array to store marks.
+ Calculate and display:
+ Total marks of each student.
+ Average marks of each subject.
+ The topper student (highest total marks).*/
 #include <iostream>
 using namespace std;
 
