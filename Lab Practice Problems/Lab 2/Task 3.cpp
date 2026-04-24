4d7cdb98ad2ac14fe4f77ebe8fd3e6ea78132876
Syed Zia Ur Rehman Hashmi <139067873+zia19068@users.noreply.github.com>
2026-04-24 21:21:59 +0500

    Update Task 3.cpp

--- Lab Practice Problems/Lab 2/Task 3.cpp
+++ Lab Practice Problems/Lab 2/Task 3.cpp
@@ -1,3 +1,9 @@
+/*Task#3
+A university is managing the marks of students in multiple subjects. Each department has a
+different number of students. Each student has marks for a fixed number of 5 subjects.
+You need to store this data in a appropriate dynamic array where rows = number of departments
+and columns = number of students in each department. Then, calculate the highest, lowest and
+average marks of each department and display them.*/
 #include <iostream>
 using namespace std;
 
