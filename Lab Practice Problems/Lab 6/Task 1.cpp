2f0ef664319a0f7501228068b2c969fdd8b4f49f
Syed Zia Ur Rehman Hashmi <139067873+zia19068@users.noreply.github.com>
2026-04-24 21:11:19 +0500

    Add recursive palindrome checker in Task 1.cpp

---
+++ Lab Practice Problems/Lab 6/Task 1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool Palindromechecker(string str, int start, int end) {
+    if (start >= end) {
+        return true;
+    }
+    
+    if (str[start] != str[end]) {
+        return false;
+    }
+    
+    return Palindromechecker(str, start + 1, end - 1);
+}
+
+int main() {
+    string word;
+    int len;
+    cout << "RECURSIVE PALINDROME CHECKER" << endl;
+
+    cout << "Input: ";
+    cin >> word;
+
+    len = word.length();
+
+    if (Palindromechecker(word, 0, len - 1)) {
+        cout << "Output: Yes, it is a palindrome" << endl;
+    } else {
+        cout << "Output: No, it is not a palindrome" << endl;
+    }
+
+    return 0;
+}
