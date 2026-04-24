f3cfefbcb8e09caad9b55d3f17524f6bec86d09b
Syed Zia Ur Rehman Hashmi <139067873+zia19068@users.noreply.github.com>
2026-04-24 21:12:42 +0500

    Implement stack using linked list in Task 1.cpp

---
+++ Lab Practice Problems/Lab 7/Task 1.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = NULL;
+    }
+};
+
+class Stack {
+private:
+    Node* top;
+
+public:
+    Stack() {
+        top = NULL;
+    }
+
+    void push(int value) {
+        Node* newNode = new Node(value);
+        newNode->next = top;
+        top = newNode;
+    }
+
+    void pop() {
+        if (top == NULL) {
+            cout << "Stack Underflow" << endl;
+            return;
+        }
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+    }
+
+    int peek() {
+        if (top == NULL) {
+            return -1;
+        }
+        return top->data;
+    }
+
+    void display() {
+        if (top == NULL) {
+            cout << "Stack is empty" << endl;
+            return;
+        }
+        Node* temp = top;
+        while (temp != NULL) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+
+    ~Stack() {
+        while (top != NULL) {
+            Node* temp = top;
+            top = top->next;
+            delete temp;
+        }
+    }
+};
+
+int main() {
+    Stack s;
+
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    cout << "Stack elements: ";
+    s.display();
+
+    cout << "Top element: " << s.peek() << endl;
+
+    s.pop();
+    cout << "After pop: ";
+    s.display();
+
+    return 0;
+}
